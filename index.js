c153d8a436acc652b252f6f7b1c5bfab8eedf8e8
Ethan Kieser <53837375+ekieser@users.noreply.github.com>
2019-12-02 13:40:52 -0600

    intial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+function retrieveImages(num) {
+    for (let i = 0; i < num; i++) {
+        if (num <= 50) {
+            fetch('https://dog.ceo/api/breeds/image/random')
+            .then(response => response.json())
+            .then(responseJson => logDogs(responseJson))
+            .catch(error => alert('An error occured. Try again later'))
+        }
+    };
+    if (num > 50) {
+        alert('Must be lower than 50. Try again');
+    }
+}
+
+function logDogs(responseJson) {
+    console.log(responseJson);
+}
+
+function submitForm() {
+    $('form').submit(event => {
+        event.preventDefault(); 
+        let dogNum = document.getElementById('number-entry').value;
+        retrieveImages(dogNum);
+    });
+}
+
+$(submitForm);
+
